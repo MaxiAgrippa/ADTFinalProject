1e1d593c047916dc253f6625e6195e05dd4ab03b
MaxiAgrippa <maxiagrippamail@mail.com>
2020-07-23 14:48:53 -0400

    V0.2 MongoDB Atlas API part start.

---
+++ src/MongoDBAtlasAPI/MongoDBAtlasAPIAuthentication.py
@@ -0,0 +1,48 @@
+from pymongo import MongoClient
+
+
+class MongoDBAtlasAPIAuthentication:
+
+    def __init__(self):
+        """
+        Constructor
+        provide information to connect mongoDB Atlas
+        """
+        self._username = "m220student"  # username
+        self._password = "m220password"  # password
+        self._cluster = "mflix.8tenw.gcp.mongodb.net"  # the cluster we gonna use
+        self._database = "test"  # the database
+
+    def _format_connection_url(self):
+        """
+        format the url for the connection with mongodb
+        :return: url : the url string
+        """
+        url = "mongodb+srv://" + self._username + ":" + self._password + "@" + self._cluster + "/" + self._database
+        return url
+
+    def get_mongodb_client(self):
+        """
+        connect to mongodb Atlas and retunr the connection object
+        :return: client the connection object with mongodb Atlas
+        """
+        try:
+            client = MongoClient(self._format_connection_url())
+        except Exception as e:
+            print(e)
+        return client
+
+
+def main():
+    """
+    requre the state of the database.
+    """
+    # TEST test head SUCCESS
+    mdbaa = MongoDBAtlasAPIAuthentication()
+    client = mdbaa.get_mongodb_client()
+    # print client state
+    print(client.state)
+
+
+if __name__ == '__main__':
+    main()
