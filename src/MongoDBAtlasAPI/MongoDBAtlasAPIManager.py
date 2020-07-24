a1fbb8da16d57a1e2f6bf7f3cf5beda76349c548
MaxiAgrippa <maxiagrippamail@mail.com>
2020-07-24 16:32:37 -0400

    V0.2 MongoDB Atlas API part: initial add data; drop; add data;

--- src/MongoDBAtlasAPI/MongoDBAtlasAPIManager.py
+++ src/MongoDBAtlasAPI/MongoDBAtlasAPIManager.py
@@ -33,14 +33,47 @@ class MongoDBAtlasAPIManager:
             print(e)
         return result
 
+    def drop_stock_price_data(self):
+        """
+        drop the StockPrice table(?)
+        """
+        client = self._authentication
+        try:
+            result = client.StockMarket.StockPrice.drop()
+        except Exception as e:
+            print(e)
+
+    def add_data_stock_price_data(self, json_data_array):
+        """
+        add data to StockPrice
+        :param json_data_array: json format data that gonna add to the document
+        :return: result: the result from connection object.
+        """
+        client = self._authentication
+        try:
+            # need to use "$each" in the json_data_array to add multiple values to the array field.
+            result = client.StockMarket.StockPrice.update_many({}, {"$addToSet": json_data_array})
+        except Exception as e:
+            print(e)
+        return result
+
 
 def main():
     mdbaapim = MongoDBAtlasAPIManager()
     # TEST: Connection SUCCESS
     print(mdbaapim.get_state())
-    # TEST: Insert data SUCCESS
-    result = mdbaapim.initial_store_stock_price_data([{"A": 1.2, "B": 2.3}])
-    print(result)
+    # TEST: Initial insert data SUCCESS
+    # result = mdbaapim.initial_store_stock_price_data([{"prices": [
+    #     {"data": 20200724, "open": 1.2, "close": 2.3, "volume": 1000}], "id": 111, "timeZone": {"gmtOffset": -14400}}])
+    # print(result)
+    # TEST: drop SUCCESS
+    # print(mdbaapim.drop_stock_price_data())
+    # TEST: add data SUCCESS
+    # need to use "$each" with the parameters to add multiple values to the array field.
+    result2 = mdbaapim.add_data_stock_price_data(
+        {"prices": {"$each": [{"data": 20200725, "open": 2.2, "close": 3.3, "volume": 1000},
+                              {"data": 20200726, "open": 3.2, "close": 4.3, "volume": 1000}]}})
+    print(result2)
 
 
 if __name__ == '__main__':
