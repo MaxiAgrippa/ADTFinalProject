31335d1b4035b4681a39580952543f7821525c77
MaxiAgrippa <maxiagrippamail@mail.com>
2020-07-30 16:56:09 -0400

    V0.2 MongoDB Atlas API part: initial add data; drop; add data;(need to fix something) V0.3 Data collector start. V0.5 Schema changed, heavy logic chang about MongoDBAtlasAPIManager.py(duplication problem not under control).

--- src/MongoDBAtlasAPI/MongoDBAtlasAPIManager.py
+++ src/MongoDBAtlasAPI/MongoDBAtlasAPIManager.py
@@ -1,4 +1,5 @@
 from src.MongoDBAtlasAPI.MongoDBAtlasAPIAuthentication import *
+import pprint
 
 
 class MongoDBAtlasAPIManager:
@@ -17,11 +18,10 @@ class MongoDBAtlasAPIManager:
         """
         return self._authentication.state
 
-    # TEST TEMPORARY METHOD, not decided.
-    def initial_store_stock_price_data(self, company, json_data_array):
+    def store_stock_price_data(self, company_symbol, json_data_array):
         """
-        used in store data for the first time.
-        :param company: the company(table) you want to initialize
+        used in store data.
+        :param company_symbol: the company(table) you want to initialize
         :param json_data_array: json data array(document array)
         :return: result: the result from connection object.
         """
@@ -30,56 +30,77 @@ class MongoDBAtlasAPIManager:
         try:
             # insert many document object, automatically set _id as default.
             # connection.database.collection.function()
-            result = client.StockMarket[company].insert_many(json_data_array)
+            result = client.StockMarket[company_symbol].insert_many(json_data_array)
         except Exception as e:
             print(e)
         return result
 
-    def drop_stock_price_data(self, company):
+    def search_stock_price_data(self, company_symbol, json_filter=None, json_projection=None):
         """
-        drop the StockPrice table(?)
-        :param company: the company(table) you want to drop
+        search in a company's collection for stock price data
+        :param company_symbol: the collection name also the company symbol
+        :param json_filter: a SON object specifying elements which must be present for a document to be included in the result set
+        :param json_projection: a list of field names that should be returned in the result set or a dict specifying the fields to include or exclude.
+        :return: result: the result from connection object.
         """
         # rename the authentication
         client = self._authentication
         try:
-            result = client.StockMarket[company].drop()
+            # find document objects inside the company collection.
+            # connection.database.collection.function()
+            result = client.StockMarket[company_symbol].find(json_filter, json_projection)
+        except Exception as e:
+            print(e)
+        return result
+
+    def drop_stock_price_data(self, company_symbol):
+        """
+        drop the StockPrice table(?)
+        :param company_symbol: the company(table) you want to drop
+        """
+        # rename the authentication
+        client = self._authentication
+        try:
+            # drop that company collection.
+            client.StockMarket[company_symbol].drop()
         except Exception as e:
             print(e)
 
     # FIXME: delete duplicated data
-    def add_data_stock_price_data(self, company, json_data_array):
+    # OLD: ERROR for now
+    # FIXME: Useless?
+    def add_data_stock_price_data(self, company_symbol, json_data_array):
         """
         add data to StockPrice
-        :param company: the company(table) you want to add price data to
+        :param company_symbol: the company(table) you want to add price data to
         :param json_data_array: json format data that gonna add to the document
         :return: result: the result from connection object.
         """
         # rename the authentication
         client = self._authentication
         try:
-            if self.check_stock_price_data_existence(company):
+            if self.check_stock_price_data_existence(company_symbol):
                 # need to use "$each" in the json_data_array to add multiple values to the array field.
-                result = client.StockMarket[company].update_many({}, {"$addToSet": json_data_array})
+                result = client.StockMarket[company_symbol].update_many({}, {"$addToSet": json_data_array})
                 # TODO:
                 # delete duplicated data
                 pipline = {}
-                client.StockMarket[company].aggregate(pipline)
+                client.StockMarket[company_symbol].aggregate(pipline)
             else:
                 print("Empty collection.")
         except Exception as e:
             print(e)
         return result
 
-    def check_stock_price_data_existence(self, company):
+    def check_stock_price_data_existence(self, company_symbol):
         """
         Check if there is any data?
-        :param company: the company(table) you want to check.
+        :param company_symbol: the company(table) you want to check.
         :return: True for has data, False for empty
         """
         # rename the authentication
         client = self._authentication
-        result = client.StockMarket[company].find_one({})
+        result = client.StockMarket[company_symbol].find_one({})
         if result is not None:
             return True
         return False
@@ -89,24 +110,37 @@ def main():
     mdbaapim = MongoDBAtlasAPIManager()
     # TEST: Connection SUCCESS
     print(mdbaapim.get_state())
-    # TEST: Initial insert data SUCCESS
-    # result = mdbaapim.initial_store_stock_price_data("test", [{"prices": [
-    #     {"data": 20200724, "open": 1.2, "close": 2.3, "volume": 1000}], "id": 111, "companySymbol": "test", "timeZone": {"gmtOffset": -14400}}])
+
+    # TEST: Initial insert data SUCCESS:
+    # result = mdbaapim.store_stock_price_data("test", [
+    #     {"date": 20200724, "open": 1.2, "close": 2.3, "volume": 1000, "companySymbol": "test", "timeZone": {"gmtOffset": -14400}},
+    #     {"date": 20200725, "open": 2.2, "close": 3.3, "volume": 1001, "companySymbol": "test", "timeZone": {"gmtOffset": -14400}},
+    #     {"date": 20200726, "open": 3.2, "close": 4.3, "volume": 1002, "companySymbol": "test", "timeZone": {"gmtOffset": -14400}},
+    # ])
     # print(result)
-    # TEST: drop SUCCESS
-    print(mdbaapim.drop_stock_price_data("test"))
-    # TEST: add data SUCCESS
+
+    # TEST: drop. SUCCESS
+    # print(mdbaapim.drop_stock_price_data("test"))
+
+    # FIXME: useless?
+    # TEST: add data ERROR
     # need to use "$each" with the parameters to add multiple values to the array field.
     # result2 = mdbaapim.add_data_stock_price_data("test",
     #                                              {"prices": {"$each": [
     #                                                  {"data": 20200725, "open": 2.2, "close": 3.3, "volume": 1000},
     #                                                  {"data": 20200726, "open": 3.2, "close": 4.3, "volume": 1000}]}})
     # print(result2)
-    # TEST: check data existence SUCCESS
+
+    # TEST: check data existence. SUCCESS
     # if mdbaapim.check_stock_price_data_existence("test"):
     #     print("E")
     # else:
     #     print("N")
+
+    # TEST: search stock price data. SUCCESS
+    result3 = mdbaapim.search_stock_price_data("test", {"data": {"$gte": 20200725, "$lte": 20200726}}, {"_id": 0})
+    for r in result3:
+        print(r)
 
 
 if __name__ == '__main__':
--- src/StockPriceDataCollector.py
+++ src/StockPriceDataCollector.py
@@ -11,60 +11,136 @@ class StockPriceDataCollector:
         self._mongodb_atlas_api_manager = MongoDBAtlasAPIManager()  # get MongoDB Atlas API Manager
         self._yahoo_finance_api_manager = YahooFinanceAPIManager()  # get Yahoo Finance API Manager
 
-    def initial_add_stock_price_data(self, frequency, data_filter, start_period, end_period, company_symbol):
+    def add_stock_price_data(self, start_period, end_period, company_symbol, frequency="1d", data_filter="history"):
+        """
+        Add stock price data of a company in a period of time(epoch time) to the database
+        :param frequency: the time interval between each data unit. ()
+        :param data_filter: Allow one of following : history|div|split TODO: what's that for/means? Currently using history.
+        :param start_period: record start time
+        :param end_period: record end time
+        :param company_symbol: The company symbol, for example Microsoft: MSFT
+        :return: -1 indicate something wrong, 0 indicate normal
+        """
         # rename Yahoo Finance API Manager
         yfapim = self._yahoo_finance_api_manager
         # rename MongoDB Atlas API Manager
         mdbaapim = self._mongodb_atlas_api_manager
         # request stock prices data
         result = yfapim.get_stock_historical_price(frequency, data_filter, start_period, end_period, company_symbol)
-        # store the extracted data for the first insert
-        data = {"prices": [], "id": 0, "companySymbol": company_symbol, "timeZone": {"gmtOffset": 0}}
+        # store the data units for insertion
+        datas = []
         # check if the request result is empty or not
         if result.text is not None:
             # transform the data to json object
             json_obj = json.loads(result.text)
-            if mdbaapim.check_stock_price_data_existence(company_symbol) is False:
-                # copy id from the original data
-                data["id"] = json_obj["id"]
-                # copy timeZone from the original data
-                data["timeZone"] = json_obj["timeZone"]
-                # copy part of prices from the original data
-                for price in json_obj["prices"]:
-                    # form a price unit only use date, open, close, and volume from the original data
-                    price_unit = {"date": price["date"], "open": price["open"], "close": price["close"], "volume":
-                        price["volume"]}
-                    # store the price unit into "prices" object.
-                    data["prices"].append(price_unit)
-                # TEST:
-                print(data)
-                # using data to create the document.
-                mdbaapim.initial_store_stock_price_data(company_symbol, data)
-            else:
-                # store the extracted data for update the price part
-                price_data = {"prices": {"$each": []}}
-                # copy part of prices from the original data
-                for price in json_obj["prices"]:
-                    price_unit = {"date": price["date"], "open": price["open"], "close": price["close"], "volume":
-                        price["volume"]}
-                    # store the price unit into "prices" object.
-                    price_data["prices"]["$each"].append(price_unit)
-                # TEST:
-                print(price_data)
-                # using price_data to update the "prices" part
-                # FIXME: need a way to avoid duplication.
-                mdbaapim.add_data_stock_price_data(company_symbol, price_data)
-                # TODO: TO BE CONTINUE:
-
-
-
+            # copy part of prices from the original data
+            for price in json_obj["prices"]:
+                # store the extracted data for each data unit
+                data = {"date": price["date"], "open": price["open"], "close": price["close"], "volume": price[
+                    "volume"], "companySymbol": company_symbol, "timeZone": json_obj["timeZone"]}
+                # append the data unit to datas array for insert many data in one time.
+                datas.append(data)
+            # TEST:
+            print(datas)
+            # insert datas to the collection.
+            try:
+                mdbaapim.store_stock_price_data(company_symbol, datas)
+                # if something goes wrong with mongoDB Atlas, print the error.
+            except Exception as e:
+                print(e)
+                return -1
         else:
+            # If the there is no data from Yahoo Finance API Manager, show an error.
             print("Yahoo Finance API Manager request result is Empty.")
+            return -1
+        return 0
+
+    def search_stock_price_data(self, start_period, end_period, company_symbol):
+        """
+        search in the company_symbol collection for stock price data,
+        the time range is between start_period and end_period.
+        :param start_period: the begin time of the date range
+        :param end_period: the end time of the date range
+        :param company_symbol: company symbol indicate which company, eg. "MSFT"
+        :return: result from the client or None if something is wrong.
+        """
+        # rename MongoDB Atlas API Manager
+        mdbaapim = self._mongodb_atlas_api_manager
+        # if start_period is smaller than end_period, show an message and return None.
+        if start_period > end_period:
+            print("ERROR(search_stock_price_data()): start period is bigger than end period!")
+            return None
+        # build json_filter to search the data.
+        json_filter = {"date": {"$gte": start_period, "$lte": end_period}, "companySymbol": company_symbol}
+        # search the company_symbol collection.
+        try:
+            result = mdbaapim.search_stock_price_data(company_symbol, json_filter=json_filter)
+            # if something goes wrong, print the error and return None
+        except Exception as e:
+            print(e)
+            return None
+        return result
+
+    def search_stock_price_data_advance(self, start_period, end_period, company_symbol, json_projection=None):
+        """
+        search in the company_symbol collection for stock price data,
+        the time range is between start_period and end_period.
+        only show result according to the json_projection requirement.
+        :param start_period: the begin time of the date range
+        :param end_period: the end time of the date range
+        :param company_symbol: company symbol indicate which company, eg. "MSFT"
+        :param json_projection: Optional, the rules to select property from the result set.
+        :return: result from the client or None if something is wrong.
+        """
+        # rename MongoDB Atlas API Manager
+        mdbaapim = self._mongodb_atlas_api_manager
+        # if start_period is smaller than end_period, show an message and return None.
+        if start_period > end_period:
+            print("ERROR(search_stock_price_data()): start period is bigger than end period!")
+            return None
+        # build json_filter to search the data.
+        json_filter = {"date": {"$gte": start_period, "$lte": end_period}, "companySymbol": company_symbol}
+        # search the company_symbol collection.
+        try:
+            result = mdbaapim.search_stock_price_data(company_symbol, json_filter=json_filter,
+                                                      json_projection=json_projection)
+            # if something goes wrong, print the error and return None
+        except Exception as e:
+            print(e)
+            return None
+        return result
 
 
 def main():
+    # TEST: request and store stock price data for one company: SUCCESS
     spdc = StockPriceDataCollector()
-    spdc.initial_add_stock_price_data("1d", "history", "1595126182", "1595385382", "GOOGL")
+    # spdc.add_stock_price_data("1d", "history", "1595721599", "1596133812", "GOOGL")
+
+    # TEST: search stock price data for one company: SUCCESS
+    # result = spdc.search_stock_price_data(1595856600, 1595856600, "GOOGL")
+    # print(result)
+    # for r in result:
+    #     if r is not None:
+    #         print(r)
+    # result2 = spdc.search_stock_price_data(1595856500, 1595856700, "GOOGL")
+    # print(result2)
+    # for r in result2:
+    #     if r is not None:
+    #         print(r)
+    # result3 = spdc.search_stock_price_data(1595856700, 1595856500, "GOOGL")
+    # print(result3)
+    # if result3 is not None:
+    #     for r in result3:
+    #         if r is not None:
+    #             print(r)
+
+    # TEST: search stock price data advance for one company: SUCCESS
+    result4 = spdc.search_stock_price_data_advance(1595856500, 1595856700, "GOOGL", {"_id": 0})
+    print(result4)
+    if result4 is not None:
+        for r in result4:
+            if r is not None:
+                print(r)
 
 
 if __name__ == '__main__':
