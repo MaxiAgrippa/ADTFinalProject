9f9ae9a340987f64f425195a42d2d3eb1171cf86
sharm192 <sharm192@uwindsor.ca>
2020-08-04 14:51:58 -0400

    google api update just getting stock data in sequence

--- src/GoogleNewsAPI/RetrivingStockInfo.py
+++ src/GoogleNewsAPI/RetrivingStockInfo.py
@@ -1,5 +1,8 @@
 import sys
 sys.path.insert(1, 'D:\Sync\Advanced Database Topics\FinalProject\Max Repo\ADTFinalProject\src\MongoDBAtlasAPI')
+import pandas as pd
+import datetime
+import time
 
 from MongoDBAtlasAPIAuthentication import MongoDBAtlasAPIAuthentication
 
@@ -7,4 +10,18 @@ if __name__ == "__main__":
     mdbaa = MongoDBAtlasAPIAuthentication()
     client = mdbaa.get_mongodb_client()
     # print client state
-    print(client.state)
+    db=client.StockMarket
+    collection_names = db.list_collection_names() 
+    for col_num, col in enumerate(collection_names):
+            collection=db[col]
+            cursor = collection.find({},{"_id":0,"companySymbol":1,"date":1})
+            count =0
+            for document in cursor:
+                dateForOneDoc = document.get('date')
+                if (dateForOneDoc != None):
+                    #print(document.get('companySymbol'))
+                    formatedDate = datetime.datetime.fromtimestamp(dateForOneDoc).strftime('%c') 
+                    print(formatedDate)
+                                                        
+                
+            print(count)
