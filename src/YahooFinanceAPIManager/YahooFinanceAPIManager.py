eac417a5595ef6652e6489e886cd7c5260b5bed6
Daniel Lacher <carneird@uwindsor.ca>
2020-08-05 15:07:28 -0400

    Adding a new collection in the StockMarket DB for relevant financial data

--- src/YahooFinanceAPIManager/YahooFinanceAPIManager.py
+++ src/YahooFinanceAPIManager/YahooFinanceAPIManager.py
@@ -13,6 +13,7 @@ class YahooFinanceAPIManager:
         self._jsonResult = None  # result object
         self._get_historical_data_url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-historical-data"
         self._get_financials_url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-financials"
+        self._get_summary_url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary"
 
     def get_stock_historical_price(self, frequency="1d", data_filter="history", period1="1595298982", period2="1595385383",
                                    symbol="APPL"):
@@ -45,6 +46,20 @@ class YahooFinanceAPIManager:
                                   params=querystring)
         return result
 
+    def get_summary(self, region="US", symbol="AAPL"):
+        """
+        get financial summary of a company
+        :param region: the region for which the summary is retrieved
+        :param symbol: the company symbol, for example Microsoft: MSFT
+        :return: result object, you can get the content by access result.text(it's a json file string)
+        """
+        # construct query json string
+        querystring = {"region": region, "symbol": symbol}
+        # query and store result
+        result = requests.request("GET", self._get_summary_url, headers=self._authentication,
+                                  params=querystring)
+        return result
+
 
 def main():
     """
@@ -54,13 +69,22 @@ def main():
     yfapim = YahooFinanceAPIManager()
     result = yfapim.get_stock_historical_price("1d", "history", "1595126182", "1595385382", "GOOGL")
     result2 = yfapim.get_financials("GOOGL")
+    result_summary = yfapim.get_summary("US", "AAPL")
     print(result.text)
-    print("\n")
-    print(result2.text)
     print("\n")
     # TEST: using json.loads to convert the string result to a json style. SUCCESS
     json_obj = json.loads(result.text)
     print(json_obj["prices"])
+    print("\n")
+    # print(result2.text)
+    # print("\n")
+    # print(result_summary.text)
+    json_summary = json.loads(result_summary.text)
+    print(json_summary["recommendationTrend"])
+    print("\n")
+    print(json_summary["earnings"])
+    print("\n")
+    print(json_summary["financialData"])
 
 
 if __name__ == '__main__':
